558a684eafae5e887f14dc3d995129de387feb3e
TINESH GAJENDRA <gtnesh.tg@gmail.com>
2026-05-03 11:09:35 +0800

    Refactor Caesar cipher mode handling and logic

--- pyTools/caesarcip.py
+++ pyTools/caesarcip.py
@@ -1,12 +1,13 @@
 # The shift key is set here
 key = 3
-mode1 = True
+mode1 = 1
 
-def mode(bool):
-    if bool==False:
+def mode(bool1):
+    if bool1==0:
         return "Decipher"
-    elif bool==True:
+    elif bool1==1:
         return "Cipher"
+    print("the current mode is hahah:", mode1)
     
 def caesar_cipher(text, shift):
     result = ""
@@ -22,14 +23,19 @@ def caesar_cipher(text, shift):
             result += char
     return result
 
+def caesar_decipher(text, shift):
+    # Deciphering is just shifting in the opposite direction
+    return caesar_cipher(text, -shift)
+
 print(f"\n---XNerk's Caesar Cipher Tool (Default Key: {key}) ---")
 print("Type your message and press Enter.")
 print("'exit'      - quit.")
 print("'cha_key12' - change cipher key.") 
 print("'mode_1'    - switch between cipher and decipher.") 
-print(" \nCurrent mode:",mode(mode1))
+
 
 while True:
+    print(" \nCurrent mode:",mode(mode1))
     user_input = input("\nEnter message: ")
 
     
@@ -39,9 +45,18 @@ while True:
     elif user_input.lower() =='cha_key12':
         key = int(input("Enter the new key:"))
         continue
+    elif user_input.lower()=='mode_1':
+        mode1=int(input("Enter  0 - Decipher,  1 - Cipher: "))
+        continue
     else:
-        encrypted_text = caesar_cipher(user_input, key)
-        print(f"Ciphertext: {encrypted_text}")
+        if mode1==1:
+            encrypted_text = caesar_cipher(user_input, key)
+            print(f"Ciphertext: {encrypted_text}")
+        elif mode1==0:
+            decrypted_text = caesar_decipher(user_input, key)
+            print(f"deciphertext: {decrypted_text}")
+
+        
         
         
  
